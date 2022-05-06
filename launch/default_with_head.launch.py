9a8eba6ec342607a61550d8bf92e0a3568b3ab32
Michael Schmidpeter <m4ffle@googlemail.com>
2022-05-06 22:50:30 +0200

    added new launchfile with arm_head

--- launch/default_with_head.launch.py
+++ launch/default_with_head.launch.py
@@ -8,11 +8,11 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-  # config_servo = os.path.join(
-  #       get_package_share_directory('francor_manipulator'),
-  #       'config',
-  #       'servo_arm_head.xml'
-  #       )
+  config_servo = os.path.join(
+        get_package_share_directory('francor_manipulator'),
+        'config',
+        'servo_arm_head.xml'
+        )
 
   francor_manipulator_node = Node(package='francor_manipulator',
                                   namespace='',
@@ -32,26 +32,26 @@ def generate_launch_description():
                                   ]
                                  ) 
 
-  # servo_lx16a = Node(package='francor_servo_lx16a',
-  #                    namespace='',
-  #                    executable='francor_servo_lx16a_node',
-  #                    name='francor_servo_lx16a_node',
-  #                    output='screen',
-  #                   #  parameters=[config],
-  #                    parameters=[{
-  #                      "servo_xml_cfg"  : config_servo,
-  #                      "serial_device"  : "/dev/ttyUSB0",
-  #                      "rate_pos_req"   : 15.0,
-  #                      "rate_speed_req" : 0.1,
-  #                      "rate_error_req" : 0.5,
-  #                      "rate_temp_req"  : 0.5,
-  #                      "rate_vin_req"   : 0.5
-  #                    }],
-  #                    remappings=[
-  #                      #pub
-  #                     #  ('/old_topic', '/new_topic'),
-  #                    ]
-  #                    )
+  servo_lx16a = Node(package='francor_servo_lx16a',
+                     namespace='',
+                     executable='francor_servo_lx16a_node',
+                     name='francor_servo_lx16a_node',
+                     output='screen',
+                    #  parameters=[config],
+                     parameters=[{
+                       "servo_xml_cfg"  : config_servo,
+                       "serial_device"  : "/dev/ttyUSB0",
+                       "rate_pos_req"   : 15.0,
+                       "rate_speed_req" : 0.1,
+                       "rate_error_req" : 0.5,
+                       "rate_temp_req"  : 0.5,
+                       "rate_vin_req"   : 0.5
+                     }],
+                     remappings=[
+                       #pub
+                      #  ('/old_topic', '/new_topic'),
+                     ]
+                     )
 
 
 #todo micro-ros-agent?? ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyACM0 --baudrate 500000      
@@ -64,5 +64,6 @@ def generate_launch_description():
 
   return LaunchDescription([
     francor_manipulator_node,
+    servo_lx16a,
     micro_ros_agent
   ])
